94065134f011c06fa6af68eafbfcaefb52b3aebc
Rodrigo Portela <rodrigo.portela@eixox.com.br>
2017-10-13 16:17:39 -0300

    added helper method (toLong) on DigitsOnlyInterceptor

--- com.eixox/src/main/java/com/eixox/interceptors/DigitsOnlyInterceptor.java
+++ com.eixox/src/main/java/com/eixox/interceptors/DigitsOnlyInterceptor.java
@@ -1,19 +1,26 @@
 package com.eixox.interceptors;
 
+import com.eixox.Convert;
 import com.eixox.Strings;
 
 public class DigitsOnlyInterceptor implements Interceptor {
 
-	public final Object intercept(Object input) {
-		if (input == null || !(input instanceof String))
-			return input;
-		else
-			return Strings.digitsOnly((String) input);
-	}
+    public synchronized final Object intercept(Object input) {
+        if (input == null || !(input instanceof String))
+            return input;
+        else
+            return Strings.digitsOnly((String) input);
+    }
 
-	private DigitsOnlyInterceptor() {
-	}
+    private DigitsOnlyInterceptor() {
+    }
 
-	public static final DigitsOnlyInterceptor Instance = new DigitsOnlyInterceptor();
+    public static final DigitsOnlyInterceptor Instance = new DigitsOnlyInterceptor();
 
+
+    public static long toLong(Object in) {
+        return Convert.toLong(
+                new DigitsOnlyInterceptor()
+                        .intercept(in));
+    }
 }
